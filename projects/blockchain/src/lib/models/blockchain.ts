59c60cad7a9b41d7e36612572190fbf03d0b61e3
pauloneto94 <pauloneto913@gmail.com>
2019-02-11 11:04:02 -0200

    aula 01

---
+++ projects/blockchain/src/lib/models/blockchain.ts
@@ -0,0 +1,7 @@
+import { Block } from './block';
+
+export class Blockchain {
+
+    public chain: Block[];
+
+}
---
+++ projects/blockchain/src/public_api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of Blockchain
+ */
+
+export * from './lib/blockchain.service';
+export * from './lib/blockchain.component';
+export * from './lib/blockchain.module';

7f42db9c87320cfd5ae159871e04414f69a09f32
pauloneto94 <pauloneto913@gmail.com>
2019-02-15 15:47:02 -0200

    Aula 05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { TooltipModule } from 'ngx-bootstrap/tooltip'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Block, BlockchainService } from 'projects/blockchain/src/public_api';
@@ -25,7 +25,8 @@ const GenesisProvider = () => {
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot()
   ],
   providers: [{provide: 'GENESIS_BLOCK', useFactory: GenesisProvider},
               BlockchainService],
